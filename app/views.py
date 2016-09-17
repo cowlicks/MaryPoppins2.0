6dcd95edf17126c6ad6aa2eb1a1e7aeb75c1fc57
Blake Griffith <blake.a.griffith@gmail.com>
2016-09-17 16:39:33 -0700

    Add mute. Slow voice. Don't block.

--- app/views.py
+++ app/views.py
@@ -5,10 +5,16 @@ from flask import request, Response
 from app import app
 
 engine = pyttsx.init('espeak')
+engine.setProperty('rate', 140)
 
 @app.route('/say', methods=['POST'])
 def say():
     phrase = request.json['phrase']
     engine.say(phrase)
-    engine.runAndWait()
+    engine.startLoop()
+    return Response(None, 200)
+
+@app.route('/mute')
+def mute():
+    engine.stop()
     return Response(None, 200)
